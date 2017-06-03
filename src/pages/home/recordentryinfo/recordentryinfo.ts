4aea95be420cbfe1d44335fcba27ee973c1a492f
Zander Olidan <kaikaiolidan@gmail.com>
2017-06-03 14:25:20 +0800

    Added Home and Record Entry Information

---
+++ src/pages/home/recordentryinfo/recordentryinfo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-record-entry-info',
+  templateUrl: 'recordentryinfo.html'
+})
+export class RecordEntryInfoPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
