ef5201993212210443151a4f52878c5e5a0c0fa9
eric1234463 <eric1234463@gmail.com>
2017-11-08 22:06:12 +0800

    gui update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,8 +16,30 @@ export class HomePage {
 	    this.recordService.initRecords().then(records => {
 			console.log(this.records);
 	    	this.records = records;
-	    })
-
+		})
+		// let startDate = new Date();
+		// startDate.setDate(startDate.getDate() - 3);
+		// let endDate = new Date();
+		// endDate.setDate(endDate.getDate() + 3);
+		// const record = {
+		// 	visitDate: new Date(),
+		// 	title: 'Fever',
+		// 	description: 'Got cold yesterday',
+		// 	factor:[{
+		// 		fever:true
+		// 	},{
+		// 		headache:true
+		// 	}],
+		// 	doctor:{
+		// 		name:'Eric Kwong',
+		// 		location:'Wan Chai'
+		// 	},
+		// 	startDate: startDate,
+		// 	endDate : endDate,
+		// 	medicine:['a','b','c'],
+		// 	rate:3.5
+		// }
+		// this.recordService.addRecords(record);
 	}
 	goToDetail(record){
     	// delete record.doctor;
