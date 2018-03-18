5157434db516d551148cab888b37fe31bceebb8a
eric1234463 <eric1234463@gmail.com>
2018-03-18 22:29:55 +0800

    update ui

--- src/pages/qr/qr.module.ts
+++ src/pages/qr/qr.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { QRPage } from './qr';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
     declarations: [
@@ -8,6 +9,7 @@ import { QRPage } from './qr';
     ],
     imports: [
         IonicPageModule.forChild(QRPage),
+        MomentModule
     ],
 })
 export class QrPageModule { }
